216757cc642fe4cbccb153e38c12b64646dfb5b8
qwerjesus <pythonadsi@gmail.com>
2023-11-13 23:53:57 -0500

    Mapa de Invitado

--- OptimusTransport/src/views/Mapa_de_invitado.java
+++ OptimusTransport/src/views/Mapa_de_invitado.java
@@ -1,43 +1,52 @@
 package views;
 
+import javax.swing.event.MouseInputListener;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.VirtualEarthTileFactoryInfo;
+import org.jxmapviewer.input.PanMouseInputListener;
+import org.jxmapviewer.input.ZoomMouseWheelListenerCenter;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+
 
 public class Mapa_de_invitado extends javax.swing.JFrame {
     int Xmouse,Ymouse;
     
     public Mapa_de_invitado() {
         initComponents();
+        init();
         this.setLocationRelativeTo(null);
+    }
+    
+    private void init(){
+        TileFactoryInfo info = new OSMTileFactoryInfo();
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+        jXMapViewer.setTileFactory(tileFactory);
+        GeoPosition geo = new GeoPosition(10.3857259,-75.4694171);
+        jXMapViewer.setAddressLocation(geo);
+        jXMapViewer.setZoom(5);
+        
+//        crear eventos con el mouse
+        MouseInputListener mm = new PanMouseInputListener(jXMapViewer);
+        jXMapViewer.addMouseListener(mm);
+        jXMapViewer.addMouseMotionListener(mm);
+        jXMapViewer.addMouseWheelListener(new ZoomMouseWheelListenerCenter(jXMapViewer));
+        
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel2 = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jXMapViewer = new org.jxmapviewer.JXMapViewer();
         Regresar = new javax.swing.JButton();
-        Mapa = new javax.swing.JLabel();
+        comboMapType = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
-            public void mouseDragged(java.awt.event.MouseEvent evt) {
-                jPanel2MouseDragged(evt);
-            }
-        });
-        jPanel2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                jPanel2MousePressed(evt);
-            }
-        });
-        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 0, 640, 30));
-
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         Regresar.setForeground(new java.awt.Color(255, 102, 0));
         Regresar.setText("↩");
@@ -47,20 +56,53 @@ public class Mapa_de_invitado extends javax.swing.JFrame {
                 RegresarActionPerformed(evt);
             }
         });
-        jPanel1.add(Regresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 30, 30));
 
-        Mapa.setBackground(new java.awt.Color(255, 255, 255));
-        Mapa.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
-        Mapa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Mapa.png"))); // NOI18N
-        Mapa.setText("jLabel1");
-        Mapa.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                MapaMousePressed(evt);
+        comboMapType.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Vista de Calles", "Vista Satelital" }));
+        comboMapType.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboMapTypeActionPerformed(evt);
             }
         });
-        jPanel1.add(Mapa, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 670, -1));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 670, 500));
+        javax.swing.GroupLayout jXMapViewerLayout = new javax.swing.GroupLayout(jXMapViewer);
+        jXMapViewer.setLayout(jXMapViewerLayout);
+        jXMapViewerLayout.setHorizontalGroup(
+            jXMapViewerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jXMapViewerLayout.createSequentialGroup()
+                .addComponent(Regresar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 452, Short.MAX_VALUE)
+                .addComponent(comboMapType, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jXMapViewerLayout.setVerticalGroup(
+            jXMapViewerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jXMapViewerLayout.createSequentialGroup()
+                .addComponent(Regresar)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(jXMapViewerLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(comboMapType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(449, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(jXMapViewer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jXMapViewer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 670, 500));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -71,20 +113,18 @@ public class Mapa_de_invitado extends javax.swing.JFrame {
        this.dispose();
     }//GEN-LAST:event_RegresarActionPerformed
 
-    private void jPanel2MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel2MousePressed
-         Xmouse=evt.getX();
-         Ymouse=evt.getY();
-    }//GEN-LAST:event_jPanel2MousePressed
-
-    private void MapaMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_MapaMousePressed
-   
-    }//GEN-LAST:event_MapaMousePressed
-
-    private void jPanel2MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel2MouseDragged
-      int x = evt.getXOnScreen();
-      int y = evt.getYOnScreen();
-      this.setLocation(x-Xmouse, y-Ymouse);
-    }//GEN-LAST:event_jPanel2MouseDragged
+    private void comboMapTypeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboMapTypeActionPerformed
+        TileFactoryInfo info;
+        int index = comboMapType.getSelectedIndex();
+        if (index == 0) {
+            info = new OSMTileFactoryInfo();
+        }else{
+            info = new VirtualEarthTileFactoryInfo(VirtualEarthTileFactoryInfo.SATELLITE);
+        }
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+        jXMapViewer.setTileFactory(tileFactory);
+        
+    }//GEN-LAST:event_comboMapTypeActionPerformed
 
     /**
      * @param args the command line arguments
@@ -122,9 +162,9 @@ public class Mapa_de_invitado extends javax.swing.JFrame {
 //    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Mapa;
     private javax.swing.JButton Regresar;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JComboBox<String> comboMapType;
+    private javax.swing.JPanel jPanel3;
+    private org.jxmapviewer.JXMapViewer jXMapViewer;
     // End of variables declaration//GEN-END:variables
 }
